1ba326f38e9b10a78af7ef06b5bb29cd0b7bc774
vilius-stankaitis <vilius@stankaitis.com>
2023-04-25 09:01:27 +0300

    Fix about page translations

--- src/Info/About/index.tsx
+++ src/Info/About/index.tsx
@@ -1,4 +1,5 @@
 import { Page, Main, Header, Section } from '@flumens';
+import { Trans as T } from 'react-i18next';
 import './styles.scss';
 
 const { P, H } = Section;
@@ -9,37 +10,46 @@ const About = () => (
     <Main id="about">
       <Section>
         <P>
-          iRecord App is an application that enables you to get involved in
-          biological recording. You can contribute your sightings with photos,
-          GPS acquired coordinates, descriptions and other information, thus
-          providing scientists with important new biodiversity information that
-          contributes to nature conservation, planning, research and education.'
+          The LERC Wales mobile application enables you to get involved in
+          biological recording in Wales. You can contribute your sightings with
+          photos, GPS acquired coordinates, descriptions and other information,
+          thus providing scientists with important new biodiversity information
+          that contributes to nature conservation, planning, research and
+          education.
         </P>
       </Section>
 
       <Section>
         <H>Who can use the app?</H>
-        <P>
-          We encourage everyone to get involved with recording species as it is
-          very easy and quick to submit useful records without specialist
-          knowledge. It doesn&apos;t matter whether you are an amateur
-          enthusiast or a qualified biologist, iRecord App is for anyone who
-          wants to contribute to our database observations of the natural
-          environment.'
+        <P skipTranslation>
+          <T>
+            We encourage everyone to get involved with recording species as it
+            is very easy and quick to submit useful records without specialist
+            knowledge. It doesn't matter whether you are an amateur enthusiast
+            or a qualified biologist, the LERC Wales app is for anyone who wants
+            to contribute to our database observations of the natural
+            environment
+          </T>
+          .
         </P>
       </Section>
 
       <Section>
         <H>App Development</H>
-        <P>
-          This app was hand crafted with love by
+        <P skipTranslation>
+          <T>This app was developed by</T>
           <a href="https://flumens.io" style={{ whiteSpace: 'nowrap' }}>
             {' '}
             Flumens.
           </a>{' '}
-          Agency specializing in building bespoke data oriented sollutions. For
-          suggestions and feedback please do not hesitate to{' '}
-          <a href="mailto:apps%40ceh.ac.uk?subject=iRecord%20App">contact us</a>
+          <T>
+            A technical consultancy that excels at building bespoke
+            environmental science and community focussed solutions.
+          </T>
+          <T>For suggestions and feedback please do not hesitate to</T>{' '}
+          <a href="mailto:apps%40ceh.ac.uk?subject=LERC%20App">
+            <T>contact us</T>
+          </a>
           .
         </P>
       </Section>
